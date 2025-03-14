a8206573c3dbd3c4011258cb776829cba0a3f72a
fayponjp <fayponjp@gmail.com>
2025-03-14 19:32:47 +0800

    Logic changes for highlighted items, only workouts should have darker colors on-tap or click

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("click", (e) => {
     const tableBody = document.getElementById("grid");
     if (tableBody.contains(e.target)) {
-        if ((e.target.classList.contains("grid-item") || e.target.closest(".grid-item").contains(e.target))) {
+        if ((e.target.classList.contains("grid-item.workout") || e.target.closest(".grid-item.workout"))) {
             e.target.closest(".grid-item").classList.toggle("checked");
         }
     }
